93e4198d56375de6e28dd72b98e121ac37c4dfaf
Marco Metz <marco.metz@gmail.com>
2012-04-26 11:39:20 +0200

    modified:   lib/active_admin/resource/action_items.rb 	modified:   lib/active_admin/resource/naming.rb

--- lib/active_admin/resource/naming.rb
+++ lib/active_admin/resource/naming.rb
@@ -18,7 +18,7 @@ module ActiveAdmin
       # Returns the name to call this resource such as "Bank Account"
       def resource_label
         if @options[:as]
-          @options[:as]
+          I18n.t(@options[:as].downcase, :scope => [:activerecord, :models], :count => 1)
         else
            resource_name.human(:default => resource_name.gsub('::', ' ')).titleize
          end
@@ -27,7 +27,7 @@ module ActiveAdmin
       # Returns the plural version of this resource such as "Bank Accounts"
       def plural_resource_label
         if @options[:as]
-          @options[:as].pluralize
+          I18n.t(@options[:as].downcase, :scope => [:activerecord, :models], :count => 3)
         else
           resource_name.human(:count => 3, :default => resource_label.pluralize).titleize
         end
